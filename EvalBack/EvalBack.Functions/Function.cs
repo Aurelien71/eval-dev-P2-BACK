2c7d868bde18270db7a5726f4d9094c42ecc2613
Aurélien MEUNIER <aurelien.meunier@diiage.org>
2024-03-06 13:11:31 +0100

    Initial commit - Azure Functions setup

--- EvalBack/EvalBack.Functions/Function.cs
+++ EvalBack/EvalBack.Functions/Function.cs
@@ -5,16 +5,16 @@ using Microsoft.Extensions.Logging;
 
 namespace EvalBack.Functions
 {
-    public class Function1
+    public class Function
     {
         private readonly ILogger<Function1> _logger;
 
-        public Function1(ILogger<Function1> logger)
+        public Function(ILogger<Function1> logger)
         {
             _logger = logger;
         }
 
-        [Function("Function1")]
+        [Function("Function")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
